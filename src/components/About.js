7dcbb9c908d8f0cadb5d58a60adc2d26af424293
Leguminous <murayaleo911@gmail.com>
2024-11-09 16:35:52 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I'm a passionate web developer with a love for creating beautiful and functional websites.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
